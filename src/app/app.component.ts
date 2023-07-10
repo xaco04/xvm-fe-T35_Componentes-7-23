9c7ea4b95042d482dc4c560c4031c8626d4de8c6
xaco04 <xavivico04@gmail.com>
2023-07-10 10:53:05 +0200

    falta retocar el css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'problema_1';
+
+  nombre = '';
+  cif = '';
+  direccion = '';
+  grupo = '';
+
+  clientes: any[] = []; // Lista de clientes
+
+  afegirclient() {
+    const nouclient = {
+      nombre: this.nombre,
+      cif: this.cif,
+      direccion: this.direccion,
+      grupo: this.grupo
+    };
+
+    // Agregar el nuevo cliente a la lista de clientes
+    this.clientes.push(nouclient);
+
+    // Restablecer los valores de los campos de entrada
+    this.nombre = '';
+    this.cif = '';
+    this.direccion = '';
+    this.grupo = '';
+  }
 }
---
+++ src/app/client/client.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.css']
+})
+export class ClientComponent {
+  @Input() data:any;
+
+}
